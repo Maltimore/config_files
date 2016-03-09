b459c094156c4559b956bb66e42b1715b875a435
Maltimore <malte.esders@gmail.com>
2016-03-09 12:01:12 +0100

    further automatize

--- .config_scripts/install_neovim_ubuntu.sh
+++ .config_scripts/install_neovim_ubuntu.sh
@@ -2,14 +2,17 @@
 
 # install vundle by just cloning it into the .vim/bundle directory
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# remember to run :PluginInstall in vim to have vundle install the plugins
-
 # install neovim (only works on Ubuntu; no Debian packages yet)
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install -y neovim
 sudo apt-get install -y python-dev python-pip python3-dev python3-pip
+# install pip and then the neovim python package for python plugins
+sudo apt-get install -y python3-pip
+pip3 install neovim
+# use Vundle to install all plugins
+vim +PluginInstall +qall
 # use .vimrc as configuration
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
