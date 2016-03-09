27084cee34e1af49653692ffbcd5bcbf43328c26
Maltimore <malte.esders@gmail.com>
2016-03-09 11:51:26 +0100

    forcing answer yes when installing

--- .config_scripts/install_neovim_ubuntu.sh
+++ .config_scripts/install_neovim_ubuntu.sh
@@ -5,12 +5,11 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # remember to run :PluginInstall in vim to have vundle install the plugins
 
 # install neovim (only works on Ubuntu; no Debian packages yet)
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install neovim
-sudo apt-get install python-dev python-pip python3-dev python3-pip
-sudo apt-get install tmux
+sudo apt-get install -y neovim
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip
 # use .vimrc as configuration
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
