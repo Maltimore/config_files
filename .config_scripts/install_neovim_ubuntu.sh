344c66cef93236c13f060117b8b9582bc9af1a3a
maltimore <malte.esders@gmail.com>
2017-02-24 16:50:13 +0100

    added grip to automatic installation

--- .config_scripts/install_neovim_ubuntu.sh
+++ .config_scripts/install_neovim_ubuntu.sh
@@ -16,6 +16,8 @@ sudo apt-get install -y neovim
 sudo apt-get install -y python-dev python-pip python3-dev python3-pip
 # install the neovim python package for python plugins
 pip3 install neovim
+# install grip for markdown-viewer
+pip3 install grip
 # use Vundle to install all plugins, but first install cmake
 sudo apt-get install -y build-essential cmake
 # use .vimrc as configuration
