938ca76b9c16f7d57c5d3b06a8588edb9733a5b5
maltimore <malte.esders@gmail.com>
2017-08-28 15:14:24 +0200

    we need to create the py36 environment before sourcing .bashrc

--- .config_scripts/create_new_user.sh
+++ .config_scripts/create_new_user.sh
@@ -16,8 +16,9 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 
 # create py36 environment
-source ~/.bashrc
 conda create -n py36 python=3.6
+
+source ~/.bashrc
 
 # how to change the ipython color profile
 # run "ipython profile create"
