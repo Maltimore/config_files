3cd0debc239562af3b1753d597d876bca468a531
maltimore <malte.esders@gmail.com>
2017-08-28 17:00:02 +0200

    clone with ssh

--- .config_scripts/create_new_user.sh
+++ .config_scripts/create_new_user.sh
@@ -4,13 +4,15 @@ sudo apt-get -y install git-core
 
 cd ~
 git init
-git remote add origin https://github.com/Maltimore/config_files.git
+git remote add origin git@github.com/Maltimore/config_files.git
 # the following is a hacky workaround to replace all current config
 # files with the ones from the repository (normal merge impossible
 # due to untracked changes)
 git fetch origin master
 git checkout origin/master  # leaves us in headless state
-git checkout master         # back to local master branch
+git checkout master         # back to local master branch, 
+							# strangely now all commits from
+							# origin are here too
 
 git config --global user.name "maltimore"
 git config --global user.email "malte.esders@gmail.com"
