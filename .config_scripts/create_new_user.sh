6de27e5808391daccb66b2bef8ca86a0223c8bc2
maltimore <malte.esders@gmail.com>
2017-08-28 17:01:38 +0200

    ignore some git warning

--- .config_scripts/create_new_user.sh
+++ .config_scripts/create_new_user.sh
@@ -2,6 +2,11 @@
 sudo apt-get update
 sudo apt-get -y install git-core
 
+git config --global user.name "maltimore"
+git config --global user.email "malte.esders@gmail.com"
+# ignore some warning for a change to git 2.x
+git config --global push.default simple
+
 cd ~
 git init
 git remote add origin git@github.com/Maltimore/config_files.git
@@ -13,9 +18,6 @@ git checkout origin/master  # leaves us in headless state
 git checkout master         # back to local master branch, 
 							# strangely now all commits from
 							# origin are here too
-
-git config --global user.name "maltimore"
-git config --global user.email "malte.esders@gmail.com"
 
 # download and install miniconda3
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
