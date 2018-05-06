306ece6c55792305c92361154167644c3ce17d85
maltimore <malte.esders@gmail.com>
2018-05-06 21:56:42 +0200

    removed some unnecessary installs

--- .config_scripts/install_neovim_ubuntu.sh
+++ .config_scripts/install_neovim_ubuntu.sh
@@ -5,12 +5,8 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # install the xsel program to be able to copy to system clipboard with "+y
 sudo apt install -y xsel
-# no idea what i need this for
-sudo apt install -y software-properties-common
 # install neovim
 sudo apt install -y neovim
-# it's not clear to me whether this is needed
-sudo apt install -y python3-dev
 # create a conda environment specially for neovim
 conda create -y -n neovim_env python
 conda activate neovim_env
@@ -23,7 +19,6 @@ pip install flake8
 # install jedi (python comprehension library)
 pip install jedi
 # use Vundle to install all plugins, but first install cmake
-sudo apt-get install -y build-essential cmake
 nvim +PluginInstall +qall
 # use .vimrc as configuration
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
