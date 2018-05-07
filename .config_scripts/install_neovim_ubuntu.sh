27d86263e936dd5343e4d221760407cf73cd400d
maltimore <malte.esders@gmail.com>
2018-05-07 12:30:56 +0200

    updated vim plugin dependencies

--- .config_scripts/install_neovim_ubuntu.sh
+++ .config_scripts/install_neovim_ubuntu.sh
@@ -5,22 +5,29 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # install the xsel program to be able to copy to system clipboard with "+y
 sudo apt install -y xsel
+
 # install neovim
 sudo apt install -y neovim
+
 # create a conda environment specially for neovim
 conda create -y -n neovim_env python
 conda activate neovim_env
+
+# PACKAGES FOR PLUGINS
 # install the neovim python package for python plugins
 pip install neovim
-# install grip for markdown-viewer
+# markdown-viewer
 pip install grip
-# install flake8 for syntastic
+sudo apt-get install -y xdotool
+# syntastic python linting
 pip install flake8
-# install jedi (python comprehension library)
+# jedi-vim (python comprehension plugin)
 pip install jedi
-# use Vundle to install all plugins, but first install cmake
+
+# use Vundle to install all plugins
 nvim +PluginInstall +qall
-# use .vimrc as configuration
+
+# use ~/.vimrc as configuration
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
