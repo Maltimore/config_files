368864011313dff53ef312a47f8898bd59c92fd5
maltimore <malte.esders@gmail.com>
2019-08-07 14:14:00 +0200

    doc: dotfile_linking

--- .config_scripts/dotfile_linking.sh
+++ .config_scripts/dotfile_linking.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+# all this does is create symlinks from each config file listed below to $HOME/.config/<configfile>
+# no config files are being moved or deleted
+echo linking config files
+echo  
 sed -e "/^#\\|^\$/d" <<'EOF' | while read V;do echo linking $V; ln -sfT "$HOME/.config/$V" "$HOME/.$V";done
 
 ackrc
