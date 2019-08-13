d8b97c21ab78cd5692c3892b92c1ac0f69cf3636
maltimore <malte@esders.info>
2019-08-13 15:34:18 +0200

    significantly simplified installation of neovim

--- .config_scripts/install_neovim_ubuntu.sh
+++ .config_scripts/install_neovim_ubuntu.sh
@@ -1,37 +1,17 @@
 #!/bin/bash
 # there are problems if the first file I edit with neovim is with "sudo nvim"
 
-# ATTENTION
-# for the moment, conda activate neovim_env doesn't work for some reason,
-# and we need to create and activate neovim_env conda environment outside this script beforehand
-
-# install vundle by just cloning it into the .vim/bundle directory
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # install the xclip program to be able to copy to system clipboard with "+y
 sudo apt install -y xclip
 
 # install neovim
 sudo apt install -y neovim
 
-# create a conda environment specially for neovim
-conda create -y -n neovim_env python
-conda activate neovim_env
-
-# PACKAGES FOR PLUGINS
 # install the neovim python package for python plugins
-pip install pynvim
+conda install pynvim
 # markdown-viewer
 pip install grip
+# mouse compatibility
 sudo apt-get install -y xdotool
-# syntastic python linting
-pip install flake8
-# jedi-vim (python comprehension plugin)
-pip install jedi
-
-# use Vundle to install all plugins
-nvim +PluginInstall +qall
-
-# use ~/.vimrc as configuration
-mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -s ~/.vim $XDG_CONFIG_HOME/nvim
-ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+# LSP
+conda install python-language-server
